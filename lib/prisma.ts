2bf4aefe67593798ee278a67688a7fb6cfa375c5
Tyler Znoj <tyler.znoj@outlook.com>
2026-06-13 01:52:33 -0400

    Using pooled db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DIRECT_URL!,
+  connectionString: process.env.DATABASE_URL!,
 });
 
 const globalForPrisma = globalThis as unknown as {
